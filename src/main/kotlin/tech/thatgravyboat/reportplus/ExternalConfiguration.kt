9c7f4391abbdd2768c4afad66de38fa8d2a35171
ThatGravyBoat <thatgravyboat@gmail.com>
2021-10-09 17:28:31 -0230

    Updated ExternalConfiguration.kt because people dont like crashing if github is offline

--- src/main/kotlin/tech/thatgravyboat/reportplus/ExternalConfiguration.kt
+++ src/main/kotlin/tech/thatgravyboat/reportplus/ExternalConfiguration.kt
@@ -9,9 +9,8 @@ object ExternalConfiguration {
     lateinit var reportTypes: List<ReportType>
 
     fun loadData() {
-        WebUtil.fetchString("https://gist.githubusercontent.com/ThatGravyBoat/ea033f7cb668eeef2a2342f92f2c309b/raw/7082570886c274b764e8bdc07f3326d28b3941f3/reportTypes.json")?.let {
-            val config = GSON.fromJson(it, JsonConfig::class.java)
-            reportTypes = config.reportTypes
+        WebUtil.fetchString("https://raw.githubusercontent.com/ThatGravyBoat/ReportPlus/master/data.json")?.let {
+            reportTypes = try { GSON.fromJson(it, JsonConfig::class.java).reportTypes } catch (e: Exception) { listOf() }
         }
     }
 
